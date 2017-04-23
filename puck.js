f4a1e427767190fb58a2abd19e26672a943975e0
Carlo Jeske <jeske@sc-networks.com>
2017-04-23 18:10:03 +0200

    [constant] driving constantly is now possible, fixed various espruino bugs

--- puck.js
+++ puck.js
@@ -1,24 +1,59 @@
+var ON  = 0; // Needs to be inverted, because 0 is not holdable at espruino
+var OFF = 1;
+
+var RUNLOOP_FREQUENCE = 1;
+
+function Pin(pin) {
+  this.obj = pin;
+  this.value = OFF;
+  this.pwm = 0;
+}
+
 var pins = {
-  M1IN1: D1,
-  M1IN2: D2,
-  M2IN1: D28,
-  M2IN2: D29,
+  M1IN1: new Pin(D1),
+  M1IN2: new Pin(D2),
+  M2IN1: new Pin(D28),
+  M2IN2: new Pin(D29),
 };
 
-//analogWrite(pins.M1IN1, 0);
-//analogWrite(pins.M1IN2, 0);
-//analogWrite(pins.M2IN1, 0);
-//analogWrite(pins.M2IN2, 0);
+function init() {
+  var allPins = [];
 
-analogWrite(pins.M1IN1, 1);
+  for(var type in pins) {
+    if(pins.hasOwnProperty(type)) {
+      var pin = pins[type];
+      allPins.push(pin);
+      console.log(type, digitalRead(pin.obj));
+    }
+  }
 
-for(var type in pins) {
-  if(pins.hasOwnProperty(type)) {
-    var pin = pins[type];
-    console.log(type, analogRead(pin));
-    // M1IN1 NaN
-    // M1IN2 0.99975585937
-    // M2IN1 0.99975585937
-    // M2IN2 0.99975585937
+  setInterval(runloop, RUNLOOP_FREQUENCE);
+
+}
+
+var increment = 0;
+
+function runloop() {
+  for(var type in pins) {
+    if(pins.hasOwnProperty(type)) {
+      var pin = pins[type];
+      var newValue = getNewValue(pin);
+      console.log(newValue, newValue !== pin.value);
+      if(newValue !== pin.value) {
+        digitalWrite(pin.obj, newValue);
+        pin.value = newValue;
+      }
+    }
+  }
+  increment++;
+}
+
+function getNewValue(pin) {
+  if(pin.pwm && increment % pin.pwm === 0) {
+    return OFF;
+  } else {
+    return ON;
   }
 }
+
+setTimeout(init, 1000); // Needs to be delayed, because of espruino delaybug, except M1IN1
