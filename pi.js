4a7c63044e5b3e809cdf57b004aa8f9397e6dbfa
plusgut <github@webentwickler2-0.de>
2017-05-06 12:25:27 +0000

    [output] improved

--- pi.js
+++ pi.js
@@ -1,9 +1,17 @@
 #! /usr/bin/env node
 
 var Joystick     = require('joystick');
-var nodeEspruino = require('node-espruino');
-
 var stick        = new Joystick(0, 3500, 350);
+
+const RUNLOOP_INTERVAL = 300;
+var state = {};
+
+const PINS = {
+  M1IN1: 'D1',
+  M1IN2: 'D2',
+  M2IN1: 'D28',
+  M2IN2: 'D29',
+};
 
 function getMotor(number){
   const AXIS = {
@@ -39,28 +47,44 @@ function getValue(value) {
 }
 
 function getPin(motor, direction) {
-  const PINS = {
-    M1IN1: 'D1',
-    M1IN2: 'D2',
-    M2IN1: 'D28',
-    M2IN2: 'D29',
-  }
-
   return PINS[motor + direction];
 }
 
-function writeCommand(motor, direction, value) {
-  console.log('digitalWrite('+getPin(motor, direction) +', ' + value + ')');
+function writeCommand(pin, value) {
+  return 'digitalWrite(' + pin +', ' + value + ');';
+}
+
+function saveState(motor, direction, value) {
+  state[getPin(motor, direction)] = value;
+  state[getPin(motor, getOtherDirection(direction))] = 0;
 }
 
 stick.on('axis', (evt) => {
   var motor = getMotor(evt.number);
   if(motor) {
     var direction = getDirection(motor, evt.value);
-    var otherDirection = getOtherDirection(direction);
-    writeCommand(motor, direction, getValue(evt.value)) 
-    writeCommand(motor, otherDirection, 0); 
-
+    saveState(motor, direction, getValue(evt.value));
   }
-
 });
+
+setInterval(() => {
+  var result = "";
+  for(var pin in state) {
+    if(state.hasOwnProperty(pin)) {
+      result += writeCommand(pin, state[pin]);
+    }
+  }
+  console.log(result);
+},RUNLOOP_INTERVAL);
+
+function init() {
+  var result = "";
+  for(var index in PINS) {
+    if(PINS.hasOwnProperty(index)) {
+      result += 'pinMode(' + PINS[index] +',"output");';
+    }
+  }
+  console.log(result);
+}
+
+//init();
