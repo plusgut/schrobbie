0ee34e06537192d54757b55fda460e2d9b06329d
plusgut <github@webentwickler2-0.de>
2019-09-16 18:26:56 +0200

    [motor] made arming possible in parallel

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
-#include <fcntl.h>#include <stdlib.h>
+#include <fcntl.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <linux/joystick.h>
 #include <wiringPi.h>
@@ -6,6 +7,8 @@
 
 const int PWM_MIN_VALUE = 700;
 const int PWM_MAX_VALUE = 2000;
+const int ESC_LEFT_PIN = 1; // 18;
+const int ESC_RIGHT_PIN = 24; // 19;
 
 int read_event(int fd, struct js_event *event)
 {
@@ -31,20 +34,26 @@ int getPwmValue(int value) {
     return PWM_MIN_VALUE + percentage * (PWM_MAX_VALUE - PWM_MIN_VALUE);
 }
 
-void armEsc(int pin) {
-    printf("Arming procedure for %d\n", pin);
+void setPwmValueBoth(int value)
+{
+    pwmWrite(ESC_LEFT_PIN, value);
+    pwmWrite(ESC_RIGHT_PIN, value);
+}
+
+void armEsc() {
+    printf("Arming procedure is running\n");
     delay(2000);
-    pwmWrite(pin, PWM_MAX_VALUE);
+    setPwmValueBoth(PWM_MAX_VALUE);
     delay(3000);
-    pwmWrite(pin, PWM_MIN_VALUE);
+    setPwmValueBoth(PWM_MIN_VALUE);
     delay(12000);
-    pwmWrite(pin, 0);
+    setPwmValueBoth(0);
     delay(2000);
-    pwmWrite(pin, PWM_MIN_VALUE);
+    setPwmValueBoth(PWM_MIN_VALUE);
 }
 
 int main()
-{
+{ 
     printf("Start \n");
 
     struct js_event event;
@@ -54,8 +63,6 @@ int main()
 
     int left_axis = 1;
     int right_axis = 4;
-    int left_pin = 18;
-    int right_pin = 19;
 
     if (js == -1)
     {
@@ -69,31 +76,29 @@ int main()
        return -2;
     }
 
-    pwmSetClock(400);
-    pwmSetRange(1024);
+    pwmSetClock(192);
+    pwmSetRange(2000);
     pwmSetMode(0);
 
-    pinMode(left_pin, PWM_OUTPUT);
-    pinMode(right_pin, PWM_OUTPUT);
-    pwmWrite(left_pin, 0);
-    pwmWrite(right_pin, 0);
+    pinMode(ESC_LEFT_PIN, PWM_OUTPUT);
+    pinMode(ESC_LEFT_PIN, PWM_OUTPUT);
+    setPwmValueBoth(PWM_MODE_MS);
 
-    armEsc(left_pin);
-    armEsc(right_pin);
+    armEsc();
 
     printf("Arming complete\n");
 
     while (read_event(js, &event) == 0)
     {
-        if (event.type == JS_EVENT_AXIS)
+        if (event.type == JS_EVENT_AXIS && event.value <= 0)
         {
             if (event.number == left_axis)
             {
                 printf("axis: %d value: %d\n", event.number, getPwmValue(event.value));
-                pwmWrite(left_pin, getPwmValue(event.value));
+                pwmWrite(ESC_LEFT_PIN, getPwmValue(event.value));
             } else if (event.number == right_axis) {
                 printf("axis: %d value: %d\n", event.number, getPwmValue(event.value));
-                pwmWrite(right_pin, getPwmValue(event.value));
+                pwmWrite(ESC_LEFT_PIN, getPwmValue(event.value));
             }
         }
     }
